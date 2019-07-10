f1535077cb7694be42462a07e1c65a4c65334590
Nate Holland <nate@gaugeinteractive.com>
2019-07-09 22:12:33 -0500

    :sparkles: Start Scaffolding Starter Theme
    Add base configs and custom Webpack settings.

--- slate.config.js
+++ slate.config.js
@@ -4,15 +4,48 @@
 // For more information, visit https://github.com/Shopify/slate/wiki/Slate-Configuration
 
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+// Base dirs for nestable paths
+const sectionBase = 'core';
+const snippetBase = 'core';
+
+const plugins = [
+  // Allow for nestable Sections & Snippets Dirs
+  new CopyWebpackPlugin([
+    {
+      from: 'sections/**/*',
+      to: '../sections/',
+      flatten: true
+    },
+    {
+      from: 'snippets/**/*',
+      to: '../snippets/',
+      flatten: true
+    }
+  ],
+    { ignore: ['core/*'] }),
+];
 
 module.exports = {
+  // See https://github.com/Shopify/slate/blob/master/packages/slate-config/common/paths.schema.js for path schema
   'cssVarLoader.liquidPath': ['src/snippets/css-variables.liquid'],
+  'paths.theme.src.sections': `sections/${sectionBase}`,
+  'paths.theme.src.snippets': `snippets/${snippetBase}`,
   'webpack.extend': {
+    plugins,
     resolve: {
       alias: {
+        // Add any theme specific path's here
+        Utilities: path.resolve('./src/scripts/utilities'),
         jquery: path.resolve('./node_modules/jquery'),
-        'lodash-es': path.resolve('./node_modules/lodash-es'),
       },
     },
   },
+  rules: [
+    {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: ['file-loader']
+    }
+  ]
 };
